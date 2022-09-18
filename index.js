27633cb62e9ae5afec6aaaba5772362bfa437ac4
Rodrigo <rodrigo695@hotmail.com>
2022-09-18 18:13:46 -0300

    prueba de deploy 1

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ const hbs = require("hbs");
 const mysql = require("mysql2");
 const nodemailer = require("nodemailer");
 const { request } = require('express');
+const { subtle } = require('crypto');
 const app = express();
 const PORT = 3000 || 8080;
 
 
 //conexion a base de datos
 const conexion = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "lalala123",
-  database: "DOJO",
+  //host: "127.0.0.1",
+  //user: "root",
+  //password: "lalala123",
+  //database: "DOJO",
 });
 
 conexion.connect((err) => {
@@ -22,33 +23,32 @@ conexion.connect((err) => {
     console.error(`error en la conexion: ${err.stack}`)
     return;
   }
-  console.log(`conectado a la base de datos dojo`);
+  //console.log(`conectado a la base de datos dojo`);
 });
-
 
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,"public")));
 
-//motor de plantillas
+//handlebars
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views"))
 hbs.registerPartials(path.join(__dirname, "views/partials"));
 
 
-//REVISAR ESTO SI SIRVE DE ALGO
-//conexion.end();
+//GETs
 
 app.get("/", (req, res, next) => {
   res.render("index", {
-    titulo: "Dojo chugi"
+    titulo: "Dojo Cobra Kai",
+    Style: "index.css"
   })
 });
 
 app.get("/formulario", (req, res, next) => {
   res.render("formulario", {
-    titulo: "Formulario para productos"
+    titulo: "Carga de producto para la tienda"
   })
 });
 
@@ -64,14 +64,76 @@ app.get("/productos", (req, res, next) => {
   });
 });
 
+app.get ("/contacto", (req, res) =>{
+  res.render("contacto", {
+    titulo: "Formulario de contacto"
+  })
+});
+
+app.get ("/gracias", (req, res) =>{
+  res.render("gracias", {
+    titulo: "gracias"
+  })
+});
+
+app.get ("/nuestro_dojo", (req, res) =>{
+  res.render("nuestro_dojo", {
+    titulo:"nuestro dojo"
+  })
+});
+
+app.get ("/sensei", (req,res) =>{
+  res.render ("sensei", {
+    titulo: "Nuestros sensei"
+  })
+});
+
+//POSTs
+
+app.post ("/contacto", (req, res) =>{
+  const { nombre, email } = req.body;
+  if (nombre == "" || email == "") {
+    let validacion = "Completa todos los datos por favor";
+    res.render("contacto", {
+      titulo: "Formulario de contacto",
+      validacion
+    })    
+  } else{
+    //console.log(nombre);
+    //console.log(email);
+    async function envioMail(){
+      let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+          //user: "rodrigoamoretti695@gmail.com",
+          pass: "orupfhdfebclwrwd"
+        }
+      });
+      let envio = await transporter.sendMail({
+        //from: "rodrigoamoretti695@gmail.com",
+        to: `${email}`,
+        subject:"COBRA KAI NEVER DIES!",
+        html:"Lo sentimos pero no estamos tomando nuevos alumnos, quizas quieras probar en un dojo de Miyagi-do."
+      })
+        res.render("gracias", {
+          titulo: "gracias",
+          nombre,
+          email
+        });
+    }
+    envioMail()
+  }
+});
 
 app.post("/formulario", (req, res) =>{
   const {nombre, precio} = req.body;
-  console.log(nombre,precio);
+  //console.log(nombre,precio);
   if (nombre == "" || precio == "") {
     let validacion = "Rellene los campos correctamente.";
     res.render("formulario", {
-      titulo: "Formulario para productos",
+      titulo: "Formulario para proveedores",
       validacion
     })    
   } else{
@@ -93,8 +155,10 @@ app.post("/formulario", (req, res) =>{
 
 
 
+
+
 app.listen(PORT, () => {
-  console.log(`el servidor esta trabajando en el puerto ${PORT}`);
+  //console.log(`el servidor esta trabajando en el puerto ${PORT}`);
 });
 
 
