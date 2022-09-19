a73033e0981110ea7230658d2f06b190987a3490
RodrigoAmoretti <105244460+RodrigoAmoretti@users.noreply.github.com>
2022-09-19 14:36:26 -0300

    Update dojo.sql

--- dojo.sql
+++ dojo.sql
@@ -1,3 +1,5 @@
+create database dojo;
+
 use dojo;
 
 create table producto(
@@ -7,4 +9,16 @@ precio int
 );
 
 select * from dojo.producto;
+
+insert into producto values(
+(null, "karategui femenino", 7500),
+(null, "taza cobra kai", 450),
+(null, "remera femenina", 2900),
+(null, "remera unisex", 2900),
+(null, "karategui liviano", 6200)
+);
+
+delete from producto where nombre= "taza cobra kai";
+
+update dojo set precio = 5000 where nombre = "karategui liviano"
 
